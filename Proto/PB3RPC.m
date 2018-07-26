da5a5067f4b8bf6f86c86a3809734312db58ad8c
Dan Kalinin <daniil551@hotmail.com>
2018-07-26 19:47:40 +0300

    Payload writing

--- Proto/PB3RPC.m
+++ Proto/PB3RPC.m
@@ -28,7 +28,13 @@
 - (void)main {
     [self updateState:HLPOperationStateDidBegin];
     
-    
+    self.reading = [self.parent.streams.input readData:NSMutableData.data minLength:4 maxLength:4 timeout:self.parent.timeout];
+    [self.reading waitUntilFinished];
+    if (self.reading.cancelled) {
+    } else if (self.reading.errors.count > 0) {
+    } else {
+        
+    }
     
     [self updateState:HLPOperationStateDidEnd];
 }
@@ -55,7 +61,36 @@
 - (void)main {
     [self updateState:HLPOperationStateDidBegin];
     
+    PB3Payload *payload = PB3Payload.message;
+    payload.serial = self.payload.serial;
+    if (self.payload.message) {
+        GPBMessage *message = self.payload.message;
+        GPBFieldDescriptor *descriptor = [message.descriptor fieldWithNumber:15];
+        payload.needsResponse = (descriptor != nil);
+        [payload.message mergeFrom:message];
+    } else {
+        payload.responseSerial = self.payload.responseSerial;
+        if (self.payload.response) {
+            [payload.message mergeFrom:self.payload.response];
+        } else {
+            payload.error = [NSString stringWithFormat:@"%@.%i", self.payload.error.domain, (int)self.payload.error.code];
+        }
+    }
     
+    NSMutableData *payloadData = payload.data.mutableCopy;
+    
+    uint32_t length = (uint32_t)payloadData.length;
+    NSMutableData *lengthData = [NSMutableData dataWithBytes:&length length:4];
+    self.writing = [self.parent.streams.output writeData:lengthData timeout:self.parent.timeout];
+    [self.writing waitUntilFinished];
+    if (self.writing.cancelled) {
+    } else if (self.writing.errors.count > 0) {
+    } else {
+        self.writing = [self.parent.streams.output writeData:payloadData timeout:self.parent.timeout];
+        [self.writing waitUntilFinished];
+    }
+    
+    [self.errors addObjectsFromArray:self.writing.errors];
     
     [self updateState:HLPOperationStateDidEnd];
 }
